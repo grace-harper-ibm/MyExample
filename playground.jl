aafeef2b934b3930de8272de46dbd509e0e8dd84
Grace Harper <grace.harper@ibm.com>
2022-06-16 00:11:21 -0400

    got fractals in plot pane/navigator

--- playground.jl
+++ playground.jl
@@ -1,13 +1,21 @@
 
-using Base
+# # using Base
+# print("THrORNS")
 
-print("THrORNS")
+# ##
+# struct BarPNG end
+# Base.show(io::IO, ::MIME"image/png", ::BarPNG) = print(io, read(joinpath(@__DIR__, "assets", "jungseohyun.webp"), String))
 
-##
-struct BarPNG end
-Base.show(io::IO, ::MIME"image/png", ::BarPNG) =
-    print(io, read(joinpath(@__DIR__, "assets", "jungseohyun.webp"), String))
+import Fatou
+using PyPlot
+c = -0.06 + 0.67im
+nf = Fatou.juliafill(:(z^2 + $c), ∂=[-1.5, 1.5, -1, 1], N=80, n=1501, cmap="gnuplot", iter=true)
+nf
 
-BarPNG()
+pygui(false)
+figure()
+plot(Fatou.fatou(nf), bare=true)
+display(gcf())
+
 ##
 
