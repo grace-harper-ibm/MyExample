f4cf0cad2e9c9c9245af310d21ed962af7a4a038
Grace Harper <grace.harper@ibm.com>
2022-06-17 19:26:47 -0400

    lines through 82 show removing from channel while other tasks are still adding. And FLoops ensure close won't happen until for loop finishes

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,4 +1,7 @@
 # I'm frsher than a - 
+
+using FLoops
+
 hlen = 9
 hno = 10
 coreno = 4
@@ -16,12 +19,7 @@ function divide_dictionary(dictionary, hno, coreno) #unimportant for here but in
 
     return array_of_dicts
 end
-
-
-
 # pull from channel every two and start a new thread merging them until channel is empty and all other threads have died 
-
-
 
 # TODO: We could simplify the API here, because reusing dict doesnt work
 # and is unlikely to help much anyway.
@@ -47,8 +45,6 @@ function update(distribution, hyperedge)
     end
     return newdict
 end
-
-
 function build_distribution(mini_dictionary)
     distribution = Dict(tuple(zeros(hlen)...) => 1.0)
     for (hyperedge, prob) in mini_dictionary
@@ -56,14 +52,6 @@ function build_distribution(mini_dictionary)
     end
     return distribution
 end
-
-hlen = 2
-test_mini = [Dict(zeros(hlen) => 0.5, ones(hlen) => 0.5, (1, 0) => 0.5) for _ in 1:12]
-
-using FLoops
-to_merge_channel = Channel{Dict{NTuple{hlen,Int64},Float64}}(32);
-
-
 function build_mini_distributions(c::Channel)
     mini_dicts = divide_dictionary(hyperedges, hno, coreno)
     @floop for (indx, mini_dict) in enumerate(mini_dicts)
@@ -74,13 +62,26 @@ function build_mini_distributions(c::Channel)
 
 end
 
-#     mini_dicts = divide_dictionary(hyperedges, hno, coreno)
-# function build_mini_distributions_channel(mini_dicts)
-#     ch = Channel{Any}()
-#     @floop for (indx, mini_dict) in enumerate(mini_dicts)
-#         put!(ch, build_distribution(mini_dict))
-#     end
-# end
+
+mini_dicts = divide_dictionary(hyperedges, hno, coreno)
+function build_mini_distributions_channel(mini_dicts, ch)
+    @floop for (indx, mini_dict) in enumerate(mini_dicts)
+        put!(ch, build_distribution(mini_dict))
+        sleep(7)
+        print("slepted")
+    end
+    close(ch)
+end
+
+
+mini_distr_ch = Channel{Dict}(30)
+task = @async build_mini_distributions_channel(mini_dicts, mini_distr_ch)
+errormonitor(task)
+for i in mini_distr_ch
+    @show i
+end
+
+
 
 function merge_two_dicts(dict1, dict2)
     merged = typeof(dict1)()
@@ -97,7 +98,8 @@ function merge_two_dicts(dict1, dict2)
     end
     return merged
 end
-
+test_mini = [Dict(zeros(hlen) => 0.5, ones(hlen) => 0.5, (1, 0) => 0.5) for _ in 1:12]
+to_merge_channel = Channel{Dict{NTuple{hlen,Int64},Float64}}(32);
 
 input_args = []
 to_merge_dict_chan = []
@@ -109,7 +111,6 @@ for distr in Channel(build_mini_distributions)
     else
         push!(to_merge_dict_chan, merge_two_dicts(args...))  # TODO make this async 
         input_args = []
-
     end
 end
 
@@ -123,33 +124,102 @@ end
 
 
 
+# binding multiple tasks to a single channel
 
 
 
 
-t = @task begin
-    sleep(10)
-    println("TASKING")
+
+
+
+
+
+
+
+
+
+
+############# can't collapse
+"""
+# # converting to channels/tasks paradigm 
+# mini_dicts = divide_dictionary(hyperedges, hno, coreno)
+# ######
+# c = Channel(30)
+
+# task_handler_1 = @async begin
+#     sleep(3)
+#     for i in 1:10
+#         put!(c, 1)
+#     end
+# end
+
+# task_handler_2 = @async begin
+#     sleep(10)
+#     for i in 1:10
+#         put!(c, 2)
+#     end
+# end
+
+# errormonitor(task_handler_1)
+# errormonitor(task_handler_2)
+
+# my_task_handler_array = [task_handler_1, task_handler_2]
+# wait_for_tasks_to_finish = @async begin
+#     for task in my_task_handler_array
+#         wait(task)
+#     end
+# end
+
+# bind(c, wait_for_tasks_to_finish)
+
+# for i in c
+#     @show i
+# end
+
+# ########### why 
+
+
+c = Channel(30)
+task_handler_array = []
+
+for minidict in divide_dictionary(hyperedges, hno, coreno)
+    push!(task_handler_array, @async begin
+        put!(c, build_distribution(mini_dict))
+    end)
 end
 
-t1 = @task begin
-    sleep(1)
-    for i in 1:20
-        sleep(1)
-        println("running: ", i)
+wait_for_tasks_to_finish = @async begin
+    [(wait(task), errormonitor(task)) for task in task_handler_array] # TODO does errormonitor work here? 
+end;
+bind(c, wait_for_tasks_to_finish)
+
+for i in c
+    @show i
+end
+
+####
+"""
+
+# technically works but no one can pull from channel until end of floops 
+mini_dicts = divide_dictionary(hyperedges, hno, coreno)
+function build_mini_distributions_channel(mini_dicts)
+    ch = Channel{Dict}(30)
+    @floop for (indx, mini_dict) in enumerate(mini_dicts)
+        put!(ch, build_distribution(mini_dict))
     end
+    close(ch)
+    return ch
 end
 
-@async begin
-    print("in my head")
-    for i in 1:10
-        print(i, " ")
-    end
+mini_distr_ch = build_mini_distributions_channel(mini_dicts)
+for i in mini_distr_ch
+    @show i
 end
 
-for i in 1:10
-    @async begin
-        sleep(5)
-        println("I've got to go! ")
-    end
-end
+
+
+
+
+
+
+
