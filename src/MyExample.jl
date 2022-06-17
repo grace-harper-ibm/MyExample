a27c60bb0b51b9299e722f6b77bfc0780b98605f
Grace Harper <grace.harper@ibm.com>
2022-06-16 21:19:06 -0400

    multiprocessing working -- needed to set https://discourse.julialang.org/t/julia-num-threads-in-vs-code-windows-10-wsl/28794/12

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -5,11 +5,7 @@ coreno = 4
 hyperedges = Dict(tuple(rand([0, 1], 1, hlen)...) => rand() for _ in 1:hno)
 
 # section hyperedges into groups of hlen/no_cores & put results distributions in a channel
-
-##
-stateful_iter = Iterators.Stateful(hyperedges)
-##
-function divide_dictionary(dictionary, hno, coreno)
+function divide_dictionary(dictionary, hno, coreno) #unimportant for here but in the future,  anyway to yield dictionary instead of literally copying the whole thing? 
     stateful_iter = Iterators.Stateful(dictionary)
     elms_per_dict = ceil(hno / coreno)
     maxiter = floor(length(dictionary) / (elms_per_dict))
@@ -21,17 +17,55 @@ function divide_dictionary(dictionary, hno, coreno)
     return array_of_dicts
 end
 
-p = divide_dictionary(hyperedges, hno, coreno)
-totno = 0
-for elm in (p)
-    totno += length(elm)
-    println("like can you just not?")
-    println(elm)
-end
-println(totno == length(hyperedges))
-
-# const results = Channel{Tuple{Int,Float64}}(32);
 
 # pull from channel every two and start a new thread merging them until channel is empty and all other threads have died 
+mini_dicts = divide_dictionary(hyperedges, hno, coreno)
+
+
+function fake_build_distribution(dictionary, i)
+    println("length of dictionary is: $(length(dictionary))")
+    print(i)
+    return dictionary
+end
+fake_channel = []
+
+using FLoops
+
+ys = zeros(8)
+xs = [i for i in 1:8]
+function f(x)
+    sleep(5)
+    println("hello", x)
+    return x^2
+end
+@floop for i in eachindex(ys, xs)
+    ys[i] = f(xs[i])
+end
+print(ys)
+
+@floop for i in 1:10
+    sleep(1)
+    println(i)
+end
+
+
+const to_merge_channel = Channel{Dict{NTuple{hlen,Int64},Float64}}(32);
+
+@floop for (indx, mini_dict) in enumerate(mini_dicts)
+    println(indx)
+    put!(to_merge_channel, fake_build_distribution(mini_dict, indx))
+end
+
+
+print(to_merge_channel)
+for elm in to_merge_channel
+    print(elm)
+end
+
+
+
+# TODO probably need to find a way to not "pass" matrices but to lock the reference 
+
+
 
 
