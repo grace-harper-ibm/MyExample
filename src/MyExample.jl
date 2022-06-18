d096f129e1b24c4a3c96091f5c912258e1ff8705
Grace Harper <grace.harper@ibm.com>
2022-06-18 12:17:47 -0400

    pausing more complex merge in favor of potentially slower but easier to implement correctly mergesort

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -67,8 +67,6 @@ mini_dicts = divide_dictionary(hyperedges, hno, coreno)
 function build_mini_distributions_channel(mini_dicts, ch)
     @floop for (indx, mini_dict) in enumerate(mini_dicts)
         put!(ch, build_distribution(mini_dict))
-        sleep(7)
-        print("slepted")
     end
     close(ch)
 end
@@ -77,10 +75,12 @@ end
 mini_distr_ch = Channel{Dict}(30)
 task = @async build_mini_distributions_channel(mini_dicts, mini_distr_ch)
 errormonitor(task)
-for i in mini_distr_ch
-    @show i
-end
 
+
+
+# ... have this function also async, puling from to_merge_dict, 
+# see if make closing of (to_merge_dict_chan) based on (closing(Channel(build_mini_distribution)) and all threads using merge channel finished)...
+# TODO probably need to find a way to not "pass" matrices but to lock the reference 
 
 
 function merge_two_dicts(dict1, dict2)
@@ -98,35 +98,91 @@ function merge_two_dicts(dict1, dict2)
     end
     return merged
 end
-test_mini = [Dict(zeros(hlen) => 0.5, ones(hlen) => 0.5, (1, 0) => 0.5) for _ in 1:12]
-to_merge_channel = Channel{Dict{NTuple{hlen,Int64},Float64}}(32);
+
+
+
+# TO begin with, might make more sense to just wait for original mini_distributions to finish into an array (wait on threads) then mergesort 
+
+
+
+
+###########
 
 input_args = []
-to_merge_dict_chan = []
-
+to_merge_channel = Channel{Dict{NTuple{hlen,Int64},Float64}}(32);
+thread_channel = Channel()
+merging_tasks = []
 # try making this async, putting stuff into to_merge_dict_chan and later ...
-for distr in Channel(build_mini_distributions)
+for distr in mini_distr_ch
     if length(input_args) < 2
         push!(input_args, distr)
     else
-        push!(to_merge_dict_chan, merge_two_dicts(args...))  # TODO make this async 
+        new_merge_thread = @async begin
+            put!(to_merge_channel, merge_two_dicts(args...))  # TODO make this async w/ FUTURES 
+        end
+        put!(thread_channel, new_merge_thread)
         input_args = []
     end
 end
+if len(input_args > 0)
+    put!(to_merge_channel, input_args[0])
+end
 
-# ... have this function also async, puling from to_merge_dict, 
-# see if make closing of (to_merge_dict_chan) based on (closing(Channel(build_mini_distribution)) and merge_)...
+function continual_merging()
+    future_end = close_merge_channel()
+    while () # Future future_end is not done 
+        # take 
+        # take 
+        # async merge & put 
+        # take1 = None 
+        # take2 = None 
+    end
+    if () # take1 or tak2 is nonzero 
+        # put! take1/take2 
+        md1 = merge(take1, take2)
+    else
+        md1 = take!(to_merge_channel)
+    end
 
-# Maybe instead just creating channels until you get a channel that closes having produced only 1 dict? 
+    for mdict in to_merge_channel
+        md1 = merge_two_dicts(md1, mdict)
+    end
+    return md1
+
+end
+
+# Hacky way to make sure channel closes when all threads finish
+function close_merge_channel()
+    while (!isempty(mini_dict_chan))
+    end
+    while !isempty(thread_channel)
+        wait(take!(thread_channel))
+    end
+    # mini_dict_chan is empty and thrad_channel is empty 
+    close(thread_channel)
+    close(to_merge_channel)
+end
+
+function finish_merging(to_merge_channel::Channel)
+    if isempty(to_merge_channel)
+        Base._throw_not_readable
+    end
+
+end
 
 
-# TODO probably need to find a way to not "pass" matrices but to lock the reference 
 
 
+# continully pull from merge, async merge + put, 
+
+
+
+# TODO test for remaining lone input_arg
 
 # binding multiple tasks to a single channel
 
 
+test_mini = [Dict(zeros(hlen) => 0.5, ones(hlen) => 0.5, (1, 0) => 0.5) for _ in 1:12]
 
 
 
